e70281b9a549b80fa84fec9d921488e1adc7695a
Simon Metson <simonmetson@uk.ibm.com>
2020-01-23 11:29:47 +0000

    server & instructions

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # oscal-view
+
+
+You'll need python 3 (developed with Python 3.7.5), verify with `python --version`
+
+- clone repo: `git  clone git@github.ibm.com:simonmetson/oscal-view.git`
+- `cd oscal-view`
+- pull in submodules: `git submodule update --init --recursive`
+
+Launch (will open a tab in a browser):
+
+``` console
+python server.py
+```
+
+- select the OSCAL schema you want to view:
+
+http://localhost:8000/catalog
+http://localhost:8000/profile
+http://localhost:8000/component
+http://localhost:8000/ssp
+
+- TODO: write the c3.json schema
---
+++ server.py
@@ -0,0 +1,92 @@
+import argparse
+import http
+import webbrowser
+import json
+
+from collections import namedtuple
+from datetime import datetime
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+
+
+schemas = {
+    'catalog': 'OSCAL/json/schema/oscal_catalog_schema.json',
+    'profile': 'OSCAL/json/schema/oscal_profile_schema.json',
+    'component': 'OSCAL/json/schema/oscal_component_schema.json',
+    'ssp': 'OSCAL/json/schema/oscal_ssp_schema.json'
+}
+
+to_replace = '''{
+    	"title": "Example Schema",
+    	"type": "object",
+    	"properties": {
+    		"firstName": {
+    			"type": "string"
+    		},
+    		"lastName": {
+    			"type": "string"
+    		},
+    		"age": {
+    			"description": "Age in years",
+    			"type": "integer",
+    			"minimum": 0
+    		}
+    	},
+    	"required": ["firstName", "lastName"]
+    }'''
+
+def populate_schemas():
+    for k, v in schemas.items():
+        with open(v) as f:
+            schemas[k] = f.read()
+
+def statics(parts):
+    with open(f'jsonschemaviewer/{parts[0]}/{parts[1]}') as f:
+        return f.read()
+
+
+
+class SchemaHandler(BaseHTTPRequestHandler):
+    template_string = None
+
+    def do_GET(self):
+        request = urlparse(self.path)
+        parts = request.path.strip('/').rsplit('/')
+        if parts[0] == 'css':
+            self.send_content('text/css',statics(parts))
+        elif parts[0] == 'js':
+            self.send_content('text/javascript;charset=UTF-8',statics(parts))
+        else:
+            self.send_content('text/html', self.render_page(parts))
+
+    def render_page(self, parts):
+        if not self.template_string:
+            with open('jsonschemaviewer/index.html') as f:
+                template_string = f.read()
+
+        # show_me = 'catalog'
+        if parts[0] in schemas:
+            show_me = parts[0]
+            return template_string.replace(to_replace, schemas[show_me])
+
+        return template_string
+
+    def send_content(self, ctype, value):
+        self.send_response(200)
+        self.send_header('Content-type',ctype)
+        self.end_headers()
+        self.wfile.write(value.encode('utf-8'))
+
+
+
+def run():
+    populate_schemas()
+    port = 8000
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, SchemaHandler)
+    webbrowser.open_new_tab(f'http://localhost:{port}/')
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
